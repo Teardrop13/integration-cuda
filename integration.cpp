7f1f6a535e47a3e809a044e1b82f6018b7ca17fb
michal <michal.lewandowski99@gmail.com>
2022-01-26 12:49:28 +0100

    sortowanie na cpu

--- integration.cpp
+++ integration.cpp
@@ -11,11 +11,33 @@
 
 using namespace std::chrono;
 
-const long long NUMBERS = 500000;
+const long long NUMBERS = 10000;
 const float MAX = 100000;
 
-int main()
-{
+void cpu_sort(float x_list[NUMBERS]) {
+    
+    for (int i=0; i<NUMBERS; i++) {
+        float minimum = x_list[i];
+        int minimum_index = i;
+        for (int k=i; k<NUMBERS; k++) {
+            if (x_list[k] < minimum) {
+                minimum = x_list[k];
+                minimum_index = k;
+            }
+        }
+        float buf = x_list[i];
+        x_list[i] = x_list[minimum_index];
+        x_list[minimum_index] = buf;
+    }
+}
+
+void print_array(float array[NUMBERS]) {
+    for (int i=0; i<NUMBERS; i++) {
+        std::cout << array[i] << std::endl;
+    }
+}
+
+int main() {
 
     float x_list[NUMBERS];
     float y_list[NUMBERS];
@@ -24,11 +46,17 @@ int main()
     generate(x_list, y_list, NUMBERS, MAX);
     auto stop = high_resolution_clock::now();
     auto duration_generate = duration_cast<microseconds>(stop - start);
-    std::cout << NUMBERS << " generated. Time: " << duration_generate.count() / 1000. << " ms" << std::endl;
+    std::cout << NUMBERS << " generated. Time: " << duration_generate.count() / 1000. << " ms" << std::endl << std::endl;
 
     // sorting
-    std::sort(std::begin(x_list), std::end(x_list));
-    std::cout << "list sorted" << std::endl;
+    // std::sort(std::begin(x_list), std::end(x_list));
+    // std::cout << "list sorted" << std::endl;
+
+    start = high_resolution_clock::now();
+    cpu_sort(x_list);
+    stop = high_resolution_clock::now();
+    auto duration_cpu_sorting = duration_cast<microseconds>(stop - start);
+    std::cout << "cpu sorting time: " << duration_cpu_sorting.count() / 1000. << " ms" << std::endl << std::endl;
 
     // cpu
     start = high_resolution_clock::now();
